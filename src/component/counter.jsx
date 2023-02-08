ea750de0ebb14ad526ab81c7b3089a7c17629162
Vladimir Revyagin <skorabeyf@gmail.com>
2023-02-08 16:16:52 +0300

    Don't work

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -1,49 +1,63 @@
 import React, {useState} from "react"
 
-const Counter=() =>{
-  //let count = 0
-const [count, setCount] = useState(0)
-const [tags, setTags] = useState(['tag1', 'tag2', 'tag3'])
- 
+const Counter=(props) =>{
+ //console.log(props)
+//const [value, setVavue] = useState(props.value)
+const{value} = props
 
-  const formatCount =()=>{
-    return count === 0 ? "Empty" : count;
+  const formatValue =()=>{
+    return value === 0 ? "Empty" : value;
   }
   const getBadgeClass =()=>{
     let classes = "badge m-2 "
-    classes+= count === 0 ? "bg-warning" : "bg-primary"
+    classes+= value === 0 ? "bg-warning" : "bg-primary"
     return classes
   }
 
 const handleIncrement=()=>{
-setCount((prevState)=>prevState + 1)
+  console.log("handleIncrement")
+//setVavue((prevState)=>prevState + 1)
 }
 
 const handleDecrement=()=>{
-  setCount((prevState)=>prevState - 1)
+  console.log("handleDecrement")
+  //setVavue((prevState)=>prevState - 1)
   }
-  const handleTag=(id)=>{
-    //setTags(['tag1', 'tag2'])
-    setTags((prevState)=>prevState.filter(tag=>tag!==id))
+ 
+ /* const renderTags =() =>{
+    return (
+      tags.length !==0 &&
+      tags.map((tag)=>{
+        <li
+        key={tag}
+        className = "btn btn-primary btn-sm m-2"
+        onClick = {()=> handleTag(tag)}
+        >
+        {tag}
+        </li>
+      })
+    )
   }
+*/
 
   return (
-    <>
-      <ul>
-      {
-        tags.map(tag=>(<li key={tag} className="btn btn-primary btn-sm m-2" onClick={()=>handleTag(tag)}>
-          {tag}</li>))
-      }
-      </ul>
-
-      <span className={getBadgeClass()}>{formatCount()}</span>
-      <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
+    <div>
+      <span>{props.name}</span>
+      <span className={getBadgeClass()}>{formatValue()}</span>
+      <button 
+            className="btn btn-primary btn-sm m-2" 
+            onClick={()=>props.onIncrement(props.id)}>
         +
         </button>
-        <button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>
+        <button 
+            className="btn btn-primary btn-sm m-2" 
+            onClick={()=>props.onDecrement(props.id)}>
         -
         </button>
-      </>
+        <button 
+            className="btn btn-danger btn-sm m-2" 
+            onClick={()=>props.onDelete(props.id)}>delete</button> 
+      </div>
     )
 }
 
---
+++ src/component/countersList.jsx
@@ -0,0 +1,72 @@
+import React, {useState} from "react";
+import Counter from "./counter";
+
+const CountersList =() =>{
+  const initionalState = [
+    {id:0, value:0, name:"ненужная вещь"},
+    {id:1, value:4, name:"вилка"},
+    {id:2, value:0, name:"ложка"},
+    {id:3, value:0, name:"тарелка"},
+    {id:4, value:0, name:"набор минималиста"},
+  ]
+
+  const [counters, setCounters ]= useState(initionalState)
+
+  const handleDelete = (id)=>{
+    const newCounters = counters.filter(item=>item.id!==id)
+      setCounters(newCounters);
+  }
+  const handleReset =()=>{
+    setCounters(initionalState)
+    console.log('Reset')
+  } 
+  const handleUpdate =()=>{
+    const updateState = [
+      {id:0, value:1, name:"ненужная вещь"},
+      {id:1, value:0, name:"вилка"},
+      {id:2, value:2, name:"ложка"},
+      {id:3, value:3, name:"тарелка"},
+      {id:4, value:4, name:"набор минималиста"},
+    ]
+    setCounters(updateState)
+  }
+  const handleIncrement =(id)=>{
+    const newValue = initionalState.map(val=>{
+      if(id===val.id){
+        console.log(val)
+        let{id,value,name} = val
+          return {id:id, value:value+1, name}
+    }
+   })
+   console.log(newValue)
+    // setCounters(newValue)
+       
+
+  }
+  const handleDecrement =(id)=>{
+    console.log("Dec")
+  }
+  return (
+      <>
+        {counters.map(count=>(
+          <Counter key={count.id} 
+                    onDelete = {handleDelete}
+                    onIncrement = {handleIncrement}
+                    onDecrement = {handleDecrement}
+                    {...count}>
+          </Counter>
+        ))}
+        <button 
+            className="btn btn-primary btn-sm m-2"
+           
+            onClick={handleUpdate}
+            
+            >
+              Сброс
+        </button>
+              </>
+  )
+}
+
+
+export default CountersList
